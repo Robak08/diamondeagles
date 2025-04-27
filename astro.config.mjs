cf4e3420d1a41fb0546035f3c30b94e8a35f26f5
Robak08 <robert.hamiga@gmail.com>
2025-04-27 11:48:38 +0300

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import svelte from '@astrojs/svelte';
+
+import vercel from '@astrojs/vercel';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [svelte(), sitemap()],
+  adapter: vercel()
+});
---
+++ src/env.d.ts
@@ -0,0 +1,3 @@
+interface ImportMetaEnv {
+    readonly CMS_URL: string;
+  }
