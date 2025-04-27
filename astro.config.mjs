d0e151159437d2b724d83580be58ab6a9a15ca67
Robak08 <robert.hamiga@gmail.com>
2025-04-27 12:20:00 +0300

    tailwind setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import svelte from '@astrojs/svelte';
-
-import vercel from '@astrojs/vercel';
-
-import sitemap from '@astrojs/sitemap';
-
+import { defineConfig } from "astro/config";
+import svelte from "@astrojs/svelte";
+import tailwindcss from "@tailwindcss/vite";
+// import vercel from "@astrojs/vercel";
+import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), sitemap()],
-  adapter: vercel()
+	integrations: [svelte(), sitemap()],
+	output: "static",
+	// adapter: vercel(),
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
