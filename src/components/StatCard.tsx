527f7a6deb27468f300ab4a6e6a6fbd280421329
Robak08 <robert.hamiga@gmail.com>
2025-05-05 17:39:58 +0300

    stat counters done, about section dragt

---
+++ src/components/StatCard.tsx
@@ -0,0 +1,53 @@
+import { useRef, useState, useEffect } from "react";
+import AnimatedNumber from "@/components/ui/animated-number";
+import { useInView } from "motion/react";
+
+export function StatCard({
+	stat,
+	text,
+	duration,
+	icon,
+}: {
+	stat: number | string;
+	text: string;
+	duration: number;
+	icon?: any;
+}) {
+	const ref = useRef(null);
+	const isInView = useInView(ref);
+
+	const endNumber =
+		typeof stat === "string" ? Number(stat.replace(",", "")) : stat;
+
+	const [currentNumber, setCurrentNumber] = useState(0);
+
+	useEffect(() => {
+		if (isInView)
+			setTimeout(() => {
+				setCurrentNumber(endNumber);
+			}, 200);
+	}, [currentNumber, isInView]);
+	return (
+		<div
+			ref={ref}
+			data-slot="card"
+			className="bg-card text-card-foreground flex flex-col gap-6 shadow-sm rounded-3xl border-2 p-4 md:p-5 xl:p-8 transition hover:-translate-y-3 hover:border-primary">
+			<div className="block p-0">
+				<div className="flex flex-col gap-4">
+					<span className="relative flex justify-center items-center size-8 shrink-0 h-12 w-12 overflow-hidden rounded-full border">
+						{/* <slot name="icon" /> */}
+						{icon ? icon : ""}
+					</span>
+					<div
+						data-stat={stat}
+						className="mt-4 text-4xl md:text-3xl lg:text-4xl xl:text-5xl leading-tight font-semibold">
+						<AnimatedNumber value={currentNumber} duration={duration} />
+					</div>
+					<p className="mb-5 max-w-52 text-base lg:text-lg font-medium">
+						{text}
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
---
+++ src/components/ui/animated-number.tsx
@@ -0,0 +1,26 @@
+import NumberFlow from "@number-flow/react";
+
+export default function AnimatedNumber({
+	value,
+	duration,
+}: {
+	value: number;
+	duration: number;
+}) {
+	return (
+		<>
+			<span className="flex items-center justify-center gap-2">
+				<NumberFlow
+					value={value}
+					transformTiming={{
+						duration: duration | 750,
+						easing:
+							"linear(0, 0.0015, 0.0058 1.06%, 0.0216 2.13%, 0.0496 3.35%, 0.0907 4.71%, 0.1799 7.15%, 0.405 12.62%, 0.5112 15.36%, 0.6114, 0.6976, 0.7695 24.03%, 0.8018, 0.8306 27.07%, 0.8573, 0.8806 30.26%, 0.9018, 0.9199 33.61%, 0.936 35.36%, 0.95 37.18%, 0.962 39.08%, 0.9724 41.14%, 0.9809 43.27%, 0.9878 45.55%, 0.9974 50.79%, 1.0014 55.66%, 1.0029 61.89%, 1.0002 99.99%)",
+					}}
+					className="text-5xl font-semibold"
+					// format={{ style: "currency", currency: "USD" }}
+				/>
+			</span>
+		</>
+	);
+}
