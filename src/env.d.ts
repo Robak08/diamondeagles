846103f47b62ac5de69bff5d73a6d281f0e3586c
Robak08 <robert.hamiga@gmail.com>
2025-05-16 13:17:57 +0200

    ts types def

--- src/env.d.ts
+++ src/env.d.ts
@@ -10,7 +10,7 @@ interface ImportMeta {
 
 declare namespace App {
   interface Locals {
-    readonly website: {
+    website: {
       title: string;
       description: string;
       heroSloga: string;
