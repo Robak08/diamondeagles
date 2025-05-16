d4e37fcdecdea422950c24b4c7ce985c4ee70290
Robak08 <robert.hamiga@gmail.com>
2025-05-16 13:04:38 +0200

    server config init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,28 +2,28 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwindcss from "@tailwindcss/vite";
-// import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [react(), svelte(), sitemap()],
-	// output: "server",
-	output: "static",
+	output: "server",
+	// output: "static",
 	prefetch: {
 		defaultStrategy: "hover",
 		prefetchAll: true,
 	},
-	// adapter: vercel({
-	// 	webAnalytics: {
-	// 		enabled: true,
-	// 	},
-	// 	// CHECK isr
-	// 	// isr: true,
-	// 	// imagesConfig: {
-	// 	// 	sizes: [320, 640, 1280],
-	// 	// },
-	// }),
+	adapter: vercel({
+		// webAnalytics: {
+		// 	enabled: true,
+		// },
+		// CHECK isr
+		// isr: true,
+		// imagesConfig: {
+		// 	sizes: [320, 640, 1280],
+		// },
+	}),
 	vite: {
 		plugins: [tailwindcss()],
 	},
